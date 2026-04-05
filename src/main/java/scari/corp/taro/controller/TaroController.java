27467613f44cb364854131eb9b1273f680113fa1
Дмитрий <walbynow@gmail.com>
2026-04-05 18:05:10 +0300

    Создан эндпоинт для истории

--- src/main/java/scari/corp/taro/controller/TaroController.java
+++ src/main/java/scari/corp/taro/controller/TaroController.java
@@ -3,9 +3,13 @@ package scari.corp.taro.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import scari.corp.taro.dto.CardResponseDto;
+import scari.corp.taro.entity.TaroHistory;
 import scari.corp.taro.service.TaroService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/taro")
@@ -22,6 +26,11 @@ public class TaroController {
     @GetMapping("/random")
     public CardResponseDto randomCard() {
         return tarotService.getRandomCard();
+    }
+
+    @GetMapping("/history")
+    public List<TaroHistory> history(@RequestParam(defaultValue = "10") int limit) {
+        return tarotService.getLastReadings(limit);
     }
 
 }
