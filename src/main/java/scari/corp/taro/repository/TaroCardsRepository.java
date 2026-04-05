e2a92a320ed645caae3cde160fcf68631f7117f0
Дмитрий <walbynow@gmail.com>
2026-04-05 12:59:32 +0300

    Создан репозиторий `TaroCardsRepository` для `TaroCards`

---
+++ src/main/java/scari/corp/taro/repository/TaroCardsRepository.java
@@ -0,0 +1,13 @@
+package scari.corp.taro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import scari.corp.taro.entity.TaroCards;
+
+@Repository
+public interface TaroCardsRepository extends JpaRepository<TaroCards, Long> {
+
+    @Query(value = "SELECT * FROM tarot_cards ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    TaroCards findRandomCard();
+}
