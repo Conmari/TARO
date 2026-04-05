7241b2bc92681d45097c8d9fba892e9745223fa2
Дмитрий <walbynow@gmail.com>
2026-04-05 12:49:00 +0300

    Создана сущность для карт `TaroCards`

---
+++ src/main/java/scari/corp/taro/entity/TaroCards.java
@@ -0,0 +1,35 @@
+package scari.corp.taro.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import scari.corp.taro.enums.Arcana;
+
+@Entity
+@Table(name = "tero_cards")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaroCards {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Arcana arcana;
+
+    private String suit;
+
+    private Integer number;
+
+    @Column(length = 2000)
+    private String uprightMeaning;
+
+    @Column(length = 2000)
+    private String reversedMeaning;
+}
