493acc4c55aab0990e678548b286f7904fb9e9e6
Дмитрий <walbynow@gmail.com>
2026-04-12 11:17:15 +0300

    Обновлен `TarotController` для работы с аутентификацией и DTO - Метод `randomCard` принимает `Principal` и `HttpServletRequest` - Метод `history` принимает `Principal`, `HttpServletRequest` и возвращает `TaroHistoryResponseDto` вместо `TaroHistory`

--- src/main/java/scari/corp/taro/controller/TaroController.java
+++ src/main/java/scari/corp/taro/controller/TaroController.java
@@ -1,14 +1,16 @@
 package scari.corp.taro.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import scari.corp.taro.dto.CardResponseDto;
-import scari.corp.taro.entity.TaroHistory;
+import scari.corp.taro.dto.taro.TaroHistoryResponseDto;
 import scari.corp.taro.service.TaroService;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -16,7 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TaroController {
 
-    private final TaroService tarotService;
+    private final TaroService taroService;
 
     /**
      * Возвращает случайную карту Таро.
@@ -24,13 +26,15 @@ public class TaroController {
      * @return DTO {@link CardResponseDto}  с данными карты
      */
     @GetMapping("/random")
-    public CardResponseDto randomCard() {
-        return tarotService.getRandomCard();
+    public CardResponseDto randomCard(Principal principal,
+                                      HttpServletRequest req) {
+        return taroService.getRandomCard(principal, req);
     }
 
     @GetMapping("/history")
-    public List<TaroHistory> history(@RequestParam(defaultValue = "10") int limit) {
-        return tarotService.getLastReadings(limit);
+    public List<TaroHistoryResponseDto> history(@RequestParam(defaultValue = "10") int limit, Principal principal,
+                                                HttpServletRequest req) {
+        return taroService.getLastReadings(principal, req, limit);
     }
 
 }
