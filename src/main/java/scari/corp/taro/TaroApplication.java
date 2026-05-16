297ce8486d35779722b4573cdd89fc26722b429c
Дмитрий <walbynow@gmail.com>
2026-05-16 10:23:06 +0300

    add `@EnableCaching` in `TaroApplication`

--- src/main/java/scari/corp/taro/TaroApplication.java
+++ src/main/java/scari/corp/taro/TaroApplication.java
@@ -2,11 +2,13 @@ package scari.corp.taro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TaroApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(TaroApplication.class, args);
 	}
 
