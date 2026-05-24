c05140c64ce6d35a1e8410401327a6f4e9f2d3ba
Дмитрий <walbynow@gmail.com>
2026-05-24 15:30:42 +0300

    Выровнял отступы

--- src/main/java/scari/corp/taro/mapper/TaroMapper.java
+++ src/main/java/scari/corp/taro/mapper/TaroMapper.java
@@ -7,12 +7,12 @@ import scari.corp.taro.dto.taro.TaroHistoryResponseDto;
 import scari.corp.taro.entity.TaroCards;
 import scari.corp.taro.entity.TaroHistory;
 
-    @Mapper(componentModel = "spring")
-    public interface TaroMapper {
+@Mapper(componentModel = "spring")
+public interface TaroMapper {
 
-        @Mapping(target = "upright", source = "meanings.upright")
-        @Mapping(target = "reversed", source = "meanings.reversed")
-        CardResponseDto toCardResponseDto(TaroCards card);
+    @Mapping(target = "upright", source = "meanings.upright")
+    @Mapping(target = "reversed", source = "meanings.reversed")
+    CardResponseDto toCardResponseDto(TaroCards card);
 
-        TaroHistoryResponseDto toHistoryResponseDto(TaroHistory history);
-    }
+    TaroHistoryResponseDto toHistoryResponseDto(TaroHistory history);
+}
