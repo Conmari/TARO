db38fa0d8f3d8e7c553e69529d5d3ca3fff919ee
Дмитрий <walbynow@gmail.com>
2026-07-06 18:39:37 +0300

    Создан класс конфига для телеграмм бота и добавлена аннотация @ConfigurationPropertiesScan

--- src/main/java/scari/corp/taro/TaroApplication.java
+++ src/main/java/scari/corp/taro/TaroApplication.java
@@ -2,11 +2,13 @@ package scari.corp.taro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableScheduling
 @EnableCaching
 @EnableAsync
