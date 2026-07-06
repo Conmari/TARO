b79b44b1e7990bd35262ae5f8893e53fe62bbe22
Дмитрий <walbynow@gmail.com>
2026-07-06 18:08:10 +0300

    Созданы универсальный dto для ответов ботов и интерфейс для команд в боте

---
+++ src/main/java/scari/corp/taro/bot/dto/BotResponse.java
@@ -0,0 +1,18 @@
+package scari.corp.taro.bot.dto;
+
+import lombok.Builder;
+
+import java.util.List;
+
+/**
+ * Универсальный DTO для отправки ответов чат-команд.
+ * <p>
+ * Используется для передачи текста и структуры экранного меню
+ * из бизнес-логики в классы-адаптеры мессенджеров.
+ */
+@Builder
+public record BotResponse(
+        String destinationId,
+        String text,
+        List<String> buttons
+) {}
